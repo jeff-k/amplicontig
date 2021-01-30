9b09dfb5ecbad315027741f8ef63b6ef607cf5b2
jeff-k <root@x-0.ca>
2021-01-30 21:20:21 +0000

    count number of read pairs with matching primer sequences

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate bio;
 extern crate clap;
+extern crate csv;
 extern crate flate2;
+extern crate serde;
 
 use clap::{App, Arg};
 
@@ -8,16 +10,45 @@ use bio::io::fastq::Reader;
 
 use flate2::bufread::MultiGzDecoder;
 
+use std::collections::{BTreeMap, HashMap};
 use std::fs::File;
-
+use std::io;
 use std::io::BufReader;
+use std::str;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Record {
+    primer: String,
+    left: String,
+    right: String,
+    index: String,
+    reference: String,
+}
 
 fn main() {
     let args = App::new("extract-amplicons")
         .version("0.1.0")
-        .arg(Arg::with_name("R1").index(1))
-        .arg(Arg::with_name("R2").index(2))
+        .arg(Arg::with_name("primers").index(1))
+        .arg(Arg::with_name("R1").index(2))
+        .arg(Arg::with_name("R2").index(3))
         .get_matches();
+
+    let mut primers = HashMap::new();
+
+    let mut csv = csv::Reader::from_reader(File::open(args.value_of("primers").unwrap()).unwrap());
+    for result in csv.deserialize() {
+        let record: Record = result.unwrap();
+        println!("{:?}", record);
+        //y        let key = record.c[..21];
+        let primer = (
+            String::from(&record.left[..20]),
+            String::from(&record.right[..20]),
+        );
+        primers.insert(primer, 0);
+    }
+
     let fq1 = Reader::new(MultiGzDecoder::new(BufReader::new(
         File::open(args.value_of("R1").unwrap()).unwrap(),
     )))
@@ -28,8 +59,22 @@ fn main() {
     .records();
 
     let mut total_pairs = 0;
-    for (_r1, _r2) in fq1.zip(fq2) {
-        total_pairs += 1;
+    let mut matched = 0;
+    for (record1, record2) in fq1.zip(fq2) {
+        match (record1, record2) {
+            (Ok(r1), Ok(r2)) => {
+                total_pairs += 1;
+                let primer = (
+                    String::from_utf8(r1.seq()[..20].to_vec()).unwrap(),
+                    String::from_utf8(r2.seq()[..20].to_vec()).unwrap(),
+                );
+                match primers.get(&primer) {
+                    Some(_) => matched += 1,
+                    None => (),
+                }
+            }
+            _ => eprintln!("Not proper fastq file pair"),
+        }
     }
-    println!("{}", total_pairs);
+    println!("{}/{}", matched, total_pairs);
 }
